2680f6b5b3d6aba5511e1c82293048e4ccccde7c
mwasplund <mwasplund@gmail.com>
2020-08-07 22:40:00 -0700

    Add request for latest release

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,51 @@
 import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
+import * as thc from "typed-rest-client/HttpClient";
+import { IHeaders } from "typed-rest-client/Interfaces";
+
+interface Asset {
+  name: string;
+  url: string;
+}
+
+export interface Release {
+  name: string;
+  assets: Asset[];
+  tarball_url: string;
+  zipball_url: string;
+}
+
+async function getlatestRelease(): Promise<Release> {
+  const headers: IHeaders = {
+    Accept: "application/vnd.github.v3+json",
+  };
+
+  const GitHubApiUrl = "https://api.github.com/repos";
+  const soupRepoPath = "mwasplund/soup";
+  const url = `${GitHubApiUrl}/${soupRepoPath}/releases/latest`;
+
+  const httpClient: thc.HttpClient = new thc.HttpClient("github-api");
+  const response = await httpClient.get(url, headers);
+
+  if (response.message.statusCode !== 200) {
+    throw new Error(`${url} failed: ${response.message.statusCode}`);
+  }
+
+  const responseBody = await response.readBody();
+  const result: Release = JSON.parse(responseBody.toString());
+
+  return result;
+}
 
 export async function run(): Promise<void> {
   try {
-    // Load the requested version and attempt to retrieve it from the releases
     const version = core.getInput("version");
+    if (version === "latest") {
+      const latestRelease = getlatestRelease();
+      console.log(latestRelease);
+    }
+
+    // Load the requested version and attempt to retrieve it from the releases
     console.log(`Setup Soup Version: ${version}`);
 
     const url = `https://github.com/mwasplund/Soup/releases/download/${version}/Soup.zip`;
